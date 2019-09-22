b6799efafb35312d56d07aedff719057b4bc63a5
Cesar <33010096+kikhi@users.noreply.github.com>
2019-09-21 21:42:57 -0700

    Cree nueva tarea
    Agrege una parte de nueva tarea

---
+++ Lista_peliculas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista_peliculas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona ("Luis"));
+            personas.Add(new Persona ("Enrique"));
+            personas.Add(new Persona ("Juan"));
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
