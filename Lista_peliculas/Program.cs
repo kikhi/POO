737625ea7e5a069c7ad538f31e5afbdc77440b5a
Cesar <33010096+kikhi@users.noreply.github.com>
2019-09-21 22:05:49 -0700

    No funciona
    intento xd

--- Lista_peliculas/Program.cs
+++ Lista_peliculas/Program.cs
@@ -7,10 +7,20 @@ namespace Lista_peliculas
     {
         static void Main(string[] args)
         {
-            List<Persona> personas = new List<Persona>();
-            personas.Add(new Persona ("Luis"));
-            personas.Add(new Persona ("Enrique"));
-            personas.Add(new Persona ("Juan"));
+            List<Pelicula> peliculas = new List<Pelicula>();
+
+            peliculas.Add(new Pelicula ("IT"));
+            peliculas.Add(new Pelicula ("Terror bajo la tierra"));
+            peliculas.Add(new Pelicula ("La niebla"));
+            peliculas.Add(new Pelicula ("Shrek"));
+            peliculas.Add(new Pelicula ("Los increibles"));
+
+            int count = 0;
+            foreach (string element in pelicula)
+            {
+                count++;
+                Console.WriteLine($"Element #{count}: {element}");
+            }
 
             //Console.WriteLine("Hello World!");
         }
