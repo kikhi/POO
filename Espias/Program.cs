0a0bdba15dba93f29b92029ccd764652d0481ac6
kikhi <catg20g@gmail.com>
2019-09-25 19:26:30 -0700

    Ejemplo en clase
    Agrege nuevo programa

---
+++ Espias/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Espias
+{
+    class Persona
+    {
+	    public string nombre;
+	    public string apellido;
+
+	    public Persona() {
+		nombre = 'Fulano';
+		apellido = 'De tal';
+	    }
+    }
+
+    class Anonymous
+    {
+	    static public void anonimiza(Persona p) { 
+			p.nombre = 'xxxxxxxxx';
+			p.apellido = 'xxxxxxxxx';
+	    }
+        static void Main(){
+		Persona espía = new Persona();
+
+		Console.WriteLine(espía.nombre);
+		anonimiza(espía);
+		Console.WriteLine(espía.nombre);
+
+	    }
+    }
+}
