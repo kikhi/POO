2031c070ac1fc7c20e3ee87cfa9e34ec54d8b31f
kikhi <33010096+kikhi@users.noreply.github.com>
2019-10-13 22:02:11 -0700

    Agrege tareas restantes
    Falta la penultima que hicimos por que la computadora me lo robo

---
+++ Domino/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Domino
+{
+    class Domino 
+    {
+        public int a;
+        public int b;
+
+        public Domino(int a, int b){
+            this.a=a;
+            this.b=b;
+           
+        }
+        public static int operator + (Domino a, Domino b)
+        {
+            return(a.a + a.b + b.a +b.b);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Domino a = new Domino(2,0);
+            Domino b = new Domino(4,1);
+            Console.WriteLine(a + b);  
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Persona
+    {
+        protected string nombre;
+        public string Nombre{get {return nombre;}}
+        public Persona(string n){
+            nombre = n;
+            }
+        public void print(){
+            Console.WriteLine("Mi nombre es {0}", nombre);
+            }
+    }
+    class Alumno : Persona
+    {
+        protected string num_control;
+        public Alumno(string nombre, string num_control): base(nombre)
+        {
+            this.num_control = num_control;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona tom = new Persona("Tom");
+            Persona Jim = new Persona("Jim");
+
+            tom.print();
+            Jim.print();
+
+            Alumno luis = new Alumno("Luis", "1621555");
+            luis.print();
+        }
+    }
+}
