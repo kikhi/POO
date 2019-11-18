1aaecdbd08ffd2658ed1d5254c43b41aa6c3ee2e
kikhi <33010096+kikhi@users.noreply.github.com>
2019-11-18 14:07:17 -0800

    Stacks
    Continuacion de la tarea

--- stack/Program.cs
+++ stack/Program.cs
@@ -7,7 +7,16 @@ namespace stack
         private T[] elementos;
         readonly int maximo;
         private int apuntador = 0;
+
+        public Stack():this(100){}
+
+         public Stack(int size)
+        {
+            maximo = size;
+            elementos = new T[maximo];
+        }
     }
+
     class Program
     {
         static void Main(string[] args)
