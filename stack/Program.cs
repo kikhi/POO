26c59802ddcd41f7ca8b665d4422f679732a319a
kikhi <33010096+kikhi@users.noreply.github.com>
2019-11-18 13:51:27 -0800

    Continuacion stacks
    Agregue clase de stacks

--- stack/Program.cs
+++ stack/Program.cs
@@ -2,6 +2,12 @@
 
 namespace stack
 {
+    class stack<T>
+    {
+        private T[] elementos;
+        readonly int maximo;
+        private int apuntador = 0;
+    }
     class Program
     {
         static void Main(string[] args)
