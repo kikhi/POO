328214cbf61534d36169ac9bb6a713fa179d6189
kikhi <33010096+kikhi@users.noreply.github.com>
2019-11-28 18:05:54 -0800

    Update Program.cs

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Generic.Colections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Proyecto
@@ -25,10 +25,10 @@ namespace Proyecto
         static void Main(string[] args)
         {
             List<Producto> productos = new List<Producto>();
-            productos.Add(new Producto("A54sa", "Placa Arduino", 14.23));
-            productos.Add(new Producto("Dsda6", "Led Verde", 5.51));
+            productos.Add(new Producto("A54sa", "Placa Arduino", 14.23d));
+            productos.Add(new Producto("Dsda6", "Led Verde", 5.51d));
             productos.Add(new Producto("Edf51", "Laptop con pantalla LCD", 22200.23d));
-            productos.Add(new Producto("Psda9", "Multimetro", 500.36));
+            productos.Add(new Producto("Psda9", "Multimetro", 500.36d));
 
             FileStream fs = new FileStream(@"Product.txt", FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter txtOut = new StreamWriter(fs);
