39f96df58a3e476b381601513cbb96b4fcb7be58
kikhi <33010096+kikhi@users.noreply.github.com>
2019-11-28 23:04:59 -0800

    Continuacioon proyecto
    Genera Documento de texto

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -7,13 +7,13 @@ namespace Proyecto
     class Producto
     {
         public string codigo;
-        public string description;
-        double Precio;
-        int departamento;
-        int likes;
+        public string descripcion;
+        public double precio;
+        public string departamento;
+        public int likes;
 
-        public Producto(string c, string d, double p){
-            codigo = c; descripcion = d; precio = p;
+        public Producto(string c, string d, double p, string m, int l){
+            codigo = c; descripcion = d; precio = p; departamento = m; likes = l;
         }
     }
     class productoDB
@@ -25,17 +25,20 @@ namespace Proyecto
         static void Main(string[] args)
         {
             List<Producto> productos = new List<Producto>();
-            productos.Add(new Producto("A54sa", "Placa Arduino", 14.23d));
-            productos.Add(new Producto("Dsda6", "Led Verde", 5.51d));
-            productos.Add(new Producto("Edf51", "Laptop con pantalla LCD", 22200.23d));
-            productos.Add(new Producto("Psda9", "Multimetro", 500.36d));
+            productos.Add(new Producto("A54sa", "Placa Arduino", 14.23d, "Electronica", 13));
+            productos.Add(new Producto("Dsda6", "Brazo metalico", 5.51d, "Mecanica", 54));
+            productos.Add(new Producto("Edf51", "Laptop con pantalla LCD", 22200.23d, "Computacion", 68));
+            productos.Add(new Producto("Psda9", "Multimetro", 500.36d, "Electricidad", 2));
 
             FileStream fs = new FileStream(@"Product.txt", FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter txtOut = new StreamWriter(fs);
 
             foreach(Producto p in productos){
-                txtOut.Write(p.descripcion + "");
+                txtOut.WriteLine(p.descripcion);
                 txtOut.WriteLine(p.precio);
+                txtOut.WriteLine(p.codigo);
+                txtOut.WriteLine(p.departamento);
+                txtOut.WriteLine(p.likes);
             }
             txtOut.Close();
         }
